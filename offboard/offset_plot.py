1a09540b0ef24c8f57de5bac8896a89d5de62d60
AnishJadoe <ajadoe@outlook.com>
2024-05-02 13:44:57 +0200

    Changed the type of plot to a 1D distribution

--- offboard/offset_plot.py
+++ offboard/offset_plot.py
@@ -17,7 +17,7 @@ plt.rcParams['font.family'] = 'Times New Roman'
 fig_size = (20,10)
 dpi = 250
 
-with open ('offset.json','r') as f:
+with open ('data/offset.json','r') as f:
     json_data = json.load(f)
     
     
@@ -28,52 +28,53 @@ closed_loop_data = [entry for entry in json_data if entry['type'] == 'closed_loo
 # Function to calculate the probability based on perch_success
 def calculate_probability(data):
     return np.mean([entry['perch_succesfull'] for entry in data])
+
 RECT_HEIGHT = 0.1
 y_offsets = [0,-0.05,-0.14]
 # Create figure and axis
-fig, ax = plt.subplots(1,2, figsize=fig_size)
+fig, ax = plt.subplots(1,1, figsize=fig_size)
+open_loop_hist = []
 # Plot open loop data
 for y_offset in y_offsets:
     data = []
     for entry in open_loop_data:
         if entry['y_offset'] == y_offset:
             data.append(entry)
-    prob = calculate_probability(data)
-    if y_offset == 0:
-        y_offset = 0.01
-    rectangle = Rectangle((0, -0.05), width=y_offset,height=RECT_HEIGHT,alpha=prob, color='blue', label=f'{prob*100}%')
-    ax[0].add_patch(rectangle)
-    rectangle = Rectangle((0, -0.05), width=-y_offset,height=RECT_HEIGHT,alpha=prob, color='blue')
-    ax[0].add_patch(rectangle)
+    open_loop_hist.append(calculate_probability(data))
+
+closed_loop_hist = []
 # Plot closed loop data
 for y_offset in y_offsets:
     data = []
     for entry in closed_loop_data:
         if entry['y_offset'] == y_offset:
             data.append(entry)
-    prob = calculate_probability(data)
-    if y_offset == 0:
-        y_offset = 0.01
-    rectangle = Rectangle((0, -0.05), width=y_offset,height=RECT_HEIGHT,alpha=prob, color='orange', label=f'{prob*100}%')
-    ax[1].add_patch(rectangle)
-    rectangle = Rectangle((0, -0.05), width=-y_offset,height=RECT_HEIGHT,alpha=prob, color='orange')
-    ax[1].add_patch(rectangle)
+    closed_loop_hist.append(calculate_probability(data))
 
-perching_naming = ['Open Loop', 'Closed Loop']
+ax.plot(y_offsets,closed_loop_hist,color='orange',lw=3, label="Perching w/ Tactile Feedback")
+ax.plot([0,0.05,0.14],closed_loop_hist,color='orange',lw=3)
+ax.plot(y_offsets,open_loop_hist,color='blue', lw=3, label="Perching w/o Tactile Feedback")
+ax.plot([0,0.05,0.14],open_loop_hist,color='blue',lw=3)
+
+ax.set_ylabel('Probability of Success')
+ax.set_xlabel('Y Offset [m]')
+# ax.plot([0,0],[0,1.5], color='grey',linewidth=5, label='Perching Object')
+ax.legend(fontsize=20)
+ax.set_ylim(0,1)
 # Add a horizontal bar representing the perching object
-for i in range(2):
-    ax[i].plot([0,0],[-0.5,0.5], color='grey',linewidth=5, label='Perching Object')
+# for i in range(2):
 
-    # Set labels and title
-    ax[i].set_xlabel('Y Offset [m]')
-    ax[i].set_ylabel('X [m]')
-    ax[i].set_xlim((-0.2,0.2))
-    ax[i].set_ylim((-0.2,0.2))
-    ax[i].legend(fontsize=20)
+#     # Set labels and title
+
+#     ax[i].set_ylabel('X [m]')
+#     ax[i].set_xlim((-0.2,0.2))
+#     ax[i].set_ylim((-0.2,0.2))
+#     ax[i].legend(fontsize=20)
 
 # y_ticks = np.linspace(-0.2, 0.2, 4)
 # ax[0].set_yticks(y_ticks)
 # ax[1].set_yticks(y_ticks)
 # Show the plot
 fig.tight_layout()
-plt.savefig('/home/anish/Documents/Thesis/Plots/offset_plot.png', bbox_inches='tight',format='png',dpi=dpi)
+plt.show()
+# plt.savefig('/home/anish/Documents/Thesis/Plots/offset_plot.png', bbox_inches='tight',format='png',dpi=dpi)
